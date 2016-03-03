993be60e6fe74396d8a4e5ef2803041cd189ed4b
Isabelle Schmit <schmit.isabelle@gmail.com>
2016-03-03 18:47:40 +0000

    we did things

--- site.js
+++ site.js
@@ -2,5 +2,16 @@ console.log("Hello world");
 
 $(document).ready(function() {
     console.log("rejoice - jquery is enabled!");
+    $.getJSON("https://raw.githubusercontent.com/jeremywrnr/jeremywrnr.github.io/master/weather.json", function(data) {
+        conso
+        var sum = 0;
+        var count = 0;
+        for(d in data) {
+            $('#weather').append("<li>"+d.TimeEST+": "+d.TemperatureF+"</li>");
+            sum += d.TemperatureF;
+            count++;
+        }
+        $('#average').append(sum/count);
+    });
 });
 
